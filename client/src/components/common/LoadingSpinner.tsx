f6067f549909b7aeb641a969490c89a332f298a0
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-08-27 13:02:22 +0530

    Update LoadingSpinner.tsx

--- client/src/components/common/LoadingSpinner.tsx
+++ client/src/components/common/LoadingSpinner.tsx
@@ -1,6 +1,1 @@
-// const LoadingSpinner = ({ size = "md" }) => {
-// 	const sizeClass = `loading-${size}`;
 
-// 	return <span className={`loading loading-spinner ${sizeClass}`} />;
-// };
-// export default LoadingSpinner;
